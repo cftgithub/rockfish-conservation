a5b3d756bb252deb941f3b05db38f2829f59649f
cftgithub <chiafongt@gmail.com>
2020-07-18 13:52:03 -0600

    renamed regulations file to fishing; added login field to navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,50 +4,58 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-  // link tag changes page path without reloading the entire page
-  <nav className="navbar bg-light navbar-light">
-    <div className="container-fluid">
-      <div className="navbar-header">
-        <Link className="navbar-brand" to="/">
-          Rockfish Conservation
+    // link tag changes page path without reloading the entire page
+    <nav className="navbar bg-light navbar-light">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <Link className="navbar-brand" to="/">
+            Rockfish Conservation
         </Link>
+        </div>
+        <ul className="nav nav-tabs mr-auto">
+          <li className="nav-item nav-link">
+            <Link to="/"
+              className={
+                window.location.pathname === "/" ||
+                  window.location.pathname === "/home"
+                  ? "active"
+                  : ""
+              }
+            >
+              Home</Link>
+          </li>
+          <li className="nav-item nav-link">
+            <Link to="/species"
+              className={
+                window.location.pathname === "/species" ? "active" : ""}
+            >
+              Species Identification</Link>
+          </li>
+          <li className="nav-item nav-link">
+            <Link to="/creel"
+              className={
+                window.location.pathname === "/creel" ? "active" : ""}
+            >
+              Creel</Link>
+          </li>
+          <li className="nav-item nav-link">
+            <Link to="/fishing"
+              className={
+                window.location.pathname === "/fishing" ? "active" : ""}
+            >
+              Fishing</Link>
+          </li>
+        </ul>
+        <div className="login">
+          <Link to="/login"
+            className={
+              window.location.pathname === "/login" ? "active" : ""}
+          >
+            Login
+        </Link>
+        </div>
       </div>
-      <ul className="nav nav-tabs mr-auto">
-        <li className="nav-item nav-link">
-          <Link to="/"
-            className={
-              window.location.pathname === "/" ||
-                window.location.pathname === "/home"
-                ? "active"
-                : ""
-            }
-          >
-            Home</Link>
-        </li>
-        <li className="nav-item nav-link">
-          <Link to="/species"
-            className={
-              window.location.pathname === "/species" ? "active" : ""}
-          >
-            Species Identification</Link>
-        </li>
-        <li className="nav-item nav-link">
-          <Link to="/creel"
-            className={
-              window.location.pathname === "/creel" ? "active" : ""}
-          >
-            Creel</Link>
-        </li>
-        <li className="nav-item nav-link">
-          <Link to="/regulations"
-            className={
-              window.location.pathname === "/regulations" ? "active" : ""}
-          >
-            Regulations</Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+    </nav>
   );
 }
 
