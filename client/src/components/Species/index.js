c72a90dbfa8312b9d945d0f30524cfa1f9a3a4f8
cftgithub <chiafongt@gmail.com>
2020-07-22 14:23:15 -0600

    added image cards to Species page; still needs to adjust size of each card and styling

---
+++ client/src/components/Species/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+function Species(props) {
+    return (
+        <div className="card">
+            <div className="img-card" onClick={() => props.click(props.id)}>
+                <img className="fish" alt={props.name} src={props.image} />
+            </div>
+            <div className="content">
+                <ul>
+                    <li>
+                        <strong>{props.name}</strong>
+                    </li>
+                    <li>
+                        <em>{"Status: " + props.status}</em>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Species;
