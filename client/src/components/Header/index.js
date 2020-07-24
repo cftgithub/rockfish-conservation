76c81bf5329afa450af30d96e1aea37f3137b735
cftgithub <chiafongt@gmail.com>
2020-07-23 23:14:45 -0600

    removed buttons from Header component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,20 +5,6 @@ function Heading() {
     return (
         <div className="hero">
             <h1 className="title">Rockfish Identification</h1>
-                {/* <button type="button" class="btn btn-primary" data-toggle="popover" data-trigger="hover"
-                    title="Good" data-content="Populations of these species seem healthy and able to withstand current fishing pressures.">Good</button>
-                <button type="button" class="btn btn-primary" data-toggle="popover" data-trigger="hover"
-                    title="Vulnerable" data-content="There is reason to believe the species may become threatened in the foreseeable future. 
-                They are susceptible to bycatch or are undergoing heavy fishing pressure and their populations have not been adequately assessed. 
-                Populations may be doing well in some regions but poor in other regions.">Vulnerable</button>
-                <button type="button" class="btn btn-primary" data-toggle="popover" data-trigger="hover"
-                    title="Threatened" data-content="Federally listed as threatened; the species is likely to become endangered in the foreseeable future.">Threatened</button>
-                <button type="button" class="btn btn-primary" data-toggle="popover" data-trigger="hover"
-                    title="Endangered" data-content="Federally listed as endangered; the species is in danger of extinction throughout all or most of it's habitation range.">Endangered</button>
-                <button type="button" class="btn btn-primary" data-toggle="popover" data-trigger="hover"
-                    title="Unknown" data-content="We simply do not know how these species are doing. 
-                    Rockfish are difficult to study and it is not always possible to get an assessment of specific species populations. 
-                    This is perhaps the worst category since we could think there is no problem when there is really a major one!">Unknown</button> */}
         </div>
     );
 }
