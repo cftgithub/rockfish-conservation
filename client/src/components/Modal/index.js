16d738a58ea53cd6d74a448762ef5355a5c68ec1
cftgithub <chiafongt@gmail.com>
2020-07-25 12:50:18 -0600

    added potential code to use for modals and buttons

---
+++ client/src/components/Modal/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+// import "./style.css";
+
+function Modal(props) {
+    return (
+        <>
+        
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+            Learn More
+</button>
+
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalCenterTitle">{props.title}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+      </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Modal;
--- client/src/components/Species/index.js
+++ client/src/components/Species/index.js
@@ -1,23 +1,50 @@
 import React from "react";
+// import Modal from "../Modal";
 import "./style.css";
 
 function Species(props) {
     return (
-        <div className="card species-card">
-            <div className="img-card" onClick={() => props.click(props.id)}>
-                <img className="fish" alt={props.name} src={props.image} />
+        <>
+            <div className="card species-card">
+                <div className="img-card" onClick={() => props.click(props.id)}>
+                    <img className="fish" alt={props.name} src={props.image} />
+                </div>
+                <div className="content">
+                    <ul>
+                        <li>
+                            <strong>{props.name}</strong>
+                        </li>
+                        <li>
+                            <em>{"Status: " + props.status}</em>
+                        </li>
+                    </ul>
+                </div>
+                {/* <Modal /> */}
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                    Learn More</button>
+
+                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalCenterTitle">{props.title}</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                {props.sciName +
+                                    props.location +
+                                    props.biology}
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="content">
-                <ul>
-                    <li>
-                        <strong>{props.name}</strong>
-                    </li>
-                    <li>
-                        <em>{"Status: " + props.status}</em>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        </>
     );
 }
 
