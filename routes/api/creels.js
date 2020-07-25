83a060c4d1f0bce34c609ae63ea8a94b92e7d785
A Suicidal Cow <aaron.bertagnole@gmail.com>
2020-07-25 13:35:26 -0600

    added routes for creel

---
+++ routes/api/creels.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const creelController = require("../../controller/creelController");
+
+router.route("/").get(creelController.findAll).post(creelController.create);
+
+router
+  .route("/:id")
+  .get(creelController.findById)
+  .put(creelController.update)
+  .delete(creelController.remove);
+
+module.exports = router;
