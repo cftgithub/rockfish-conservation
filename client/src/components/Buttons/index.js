4b987b69e8c434e78bf84a8647250ab984a50d2d
msyphus <mark.syphus@gmail.com>
2020-07-29 21:43:06 -0600

    stylized popover titles to have same background as content

--- client/src/components/Buttons/index.js
+++ client/src/components/Buttons/index.js
@@ -4,54 +4,54 @@ import "./style.css";
 
 const popGood = (
   <Popover id="popGood">
-    <Popover.Title as="h3" bg>Good</Popover.Title>
     <Popover.Content>
-      Populations of these species seem healthy and able to withstand current
-      fishing pressures.
+      <h4>Good</h4>
+      <p>Populations of these species seem healthy and able to withstand current
+      fishing pressures.</p>
     </Popover.Content>
   </Popover>
 )
 const popVul = (
   <Popover id="popVul">
-    <Popover.Title as="h3">Vulnerable</Popover.Title>
     <Popover.Content>
-      There is reason to believe the species may become threatened in the
+      <h4>Vulnerable</h4>
+      <p>There is reason to believe the species may become threatened in the
       foreseeable future. They are susceptible to bycatch or are undergoing
       heavy fishing pressure and their populations have not been adequately
       assessed. Populations may be doing well in some regions but poor in other
-      regions.
+      regions.</p>
       </Popover.Content>
   </Popover>
 );
 
 const popThreat = (
   <Popover id="popThreat">
-    <Popover.Title as="h3">Threatened</Popover.Title>
     <Popover.Content>
-      Federally listed as threatened; the species is likely to become endangered
-      in the foreseeable future.
+      <h4>Threatened</h4>
+      <p>Federally listed as threatened; the species is likely to become endangered
+      in the foreseeable future.</p>
       </Popover.Content>
   </Popover>
 );
 
 const popEndanger = (
   <Popover id="popEndanger">
-    <Popover.Title as="h3">Endangered</Popover.Title>
     <Popover.Content>
-      Federally listed as endangered; the species is in danger of extinction
-      throughout all or most of it's habitation range.
+     <h4>Endangered</h4>
+      <p>Federally listed as endangered; the species is in danger of extinction
+      throughout all or most of it's habitation range.</p>
       </Popover.Content>
   </Popover>
 );
 
 const popUnk = (
   <Popover id="popUnknown">
-    <Popover.Title as="h3">Unknown</Popover.Title>
     <Popover.Content>
-      We simply do not know how these species are doing. Rockfish are difficult
+      <h4>Unknown</h4>
+      <p>We simply do not know how these species are doing. Rockfish are difficult
       to study and it is not always possible to get an assessment of specific
       species populations. This is perhaps the worst category since we could
-      think there is no problem when there is really a major one!
+      think there is no problem when there is really a major one!</p>
       </Popover.Content>
   </Popover>
 );
