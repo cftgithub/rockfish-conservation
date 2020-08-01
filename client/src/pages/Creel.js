537f40c28e22474a2f7a52bdee8870532372f631
A Suicidal Cow <aaron.bertagnole@gmail.com>
2020-07-31 20:57:26 -0600

    updated Creel.js

--- client/src/pages/Creel.js
+++ client/src/pages/Creel.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
-import DeleteBtn from "../components/DeleteBtn";
-import EditBtn from "../components/EditBtn"
-import { Input, SubmitBtn } from "../components/SubmitBtn";
+import "./style.css";
 import API from "../utils/API";
-import { List, ListItem } from "../components/List";
+import {List, ListItem } from "../components/List";
+import CreelForm from "../components/CreelForm";
+import DeleteBtn from "../components/DeleteBtn";
+import EditBtn from "../components/EditBtn";
+import Heading from "../components/Header";
+import { Card } from "react-bootstrap";
+import { Player, ControlBar, LoadingSpinner } from "video-react";
+import "../../node_modules/video-react/dist/video-react.css";
+import video from "../assets/videos/video.mp4";
 
 class Creel extends Component {
   state = {
     catches: [],
     species: "",
-    length: []
+    length: 0
   };
 
   componentDidMount() {
@@ -28,103 +34,57 @@ class Creel extends Component {
       .catch((err) => console.log(err));
   };
 
-  handleInputChange = event => {
-    const { name, value} = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    alert('New Info Was Submitted: ' + this.state.value)
-    event.preventDefault();
-    if (this.state.species && this.state.length && this.state.dateCaught && this.state.locationCaught){
-      API.saveCreel({
-        species: this.state.species,
-        length: this.state.length,
-        dateCaught: this.state.dateCaught,
-        locationCaught: this.state.locationCaught
-      })
-      .then(res => this.loadCatches())
-      .catch(err => console.log(err));
-    }
-  };
-
   render() {
     return (
-      <div className='creelpage'>
-        <div className='container creel'>
-          <h1 className='text-center'>Creel Page</h1>
-          <h4 className='text-center mb-3'>
-            Track your catches to help scientists monitor wild populations.
-          </h4>
-          <div className='card col-6 text-center float-left'>
-            <div className='card-body creel text-center'>
-              <h5 className='card-title creel text-center '>
-                Enter Rockfish data.
-              </h5>
-              <p className='card-text'></p>
-              <div className='input-group-prepend'>
-                <Input
-                  value={this.state.species}
-                  onChange={this.handleInputChange}
-                  name='species'
-                  placeholder='Species (required)'
-                />
+      <>
+        <div className='creelpage container-fluid text-center'>
+          <Player autoPlay loop muted playsInline src={video}>
+            <ControlBar disabled />
+            <LoadingSpinner disabled />
+          </Player>
+        </div>
+        <div className='col-lg-12 col-md-auto p-0 d-flex justify-content-center text-center cards'>
+          <div className='container-fluid creel'>
+            <Heading
+              title='Creel'
+              subtitle='Track your catches to help scientists monitor wild populations.'
+            />
+            <div className='row'>
+              <div className='col-6 text-center'>
+                <CreelForm />
               </div>
-              <div className='input-group-prepend'>
-                <Input
-                  value={this.state.length}
-                  onChange={this.handleInputChange}
-                  name='length'
-                  placeholder='Length (required)'
-                />
+              <div className='col-6 text-center'>
+                <Card className='catch-card'>
+                  <Card.Body>
+                    <Card.Title className='catch-title'>
+                      Catch History
+                    </Card.Title>
+                    {this.state.catches.map((caught) => {
+                      return (
+                        <List>
+                        <ListItem key={caught._id}>
+                          <a href={"/creels/" + caught._id}>
+                            <strong className='catch-text'>
+                              {caught.species} Length: {caught.length}
+                            </strong>
+                          </a>
+                          <DeleteBtn
+                            onClick={() => this.deleteCreel(caught._id)}
+                          />
+                          <EditBtn
+                            onClick={() => this.updateCreel(caught._id)}
+                          />
+                        </ListItem>
+                        </List>
+                      );
+                    })}
+                  </Card.Body>
+                </Card>
               </div>
-              <div className='input-group-prepend'>
-                <Input
-                  value={this.state.dateCaught}
-                  onChange={this.handleInputChange}
-                  name='dateCaught'
-                  placeholder='dateCaught (required)'
-                />
-              </div>
-              <div className='input-group-prepend'>
-                <Input
-                  value={this.state.locationCaught}
-                  onChange={this.handleInputChange}
-                  name='locationCaught'
-                  placeholder='Location Caught (required)'
-                />
-              </div>
-              <br></br>
-              <SubmitBtn
-                disabled={!(this.state.species && this.state.length)}
-                onClick={this.handleFormSubmit}>Submit</SubmitBtn>
-            </div>
-          </div>
-          <div className='card col-6 text-center float-left'>
-            <div className='card-body creel text-center'>
-              <h5 className='card-title creel text-center '>Catch History</h5>
-              <List>
-                {this.state.catches.map((caught) => {
-                  return (
-                    <ListItem key={caught._id}>
-                      <a href={"/creels/" + caught._id}>
-                        <strong>
-                          {caught.species} Length: {caught.length}
-                        </strong>
-                      </a>
-                      <br></br>
-                      <DeleteBtn onClick={() => this.deleteCreel(caught._id)} />
-                      <EditBtn onClick={() => this.updateCreel(caught._id)} />
-                    </ListItem>
-                  );
-                })}
-              </List>
             </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
