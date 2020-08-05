175f9e50ff64ecce553d37a8d4f1fe81d168c113
msyphus <mark.syphus@gmail.com>
2020-08-04 22:01:24 -0600

    have to login to access Creel page

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -11,28 +11,11 @@ class Profile extends Component {
     render() {
         const { user } = this.props.auth;
         return (
-            <div style={{ height: "75vh" }} className="container valign-wrapper">
+            <div>
                 <div className="row">
                     <div className="col s12 center-align">
-                        <h4>
-                            <b>Hey there,</b> {user.username}
-                            <p className="flow-text grey-text text-darken-1">
-                                You are logged into a full-stack{" "}
-                                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
-                        </h4>
-                        <button
-                            style={{
-                                width: "150px",
-                                borderRadius: "3px",
-                                letterSpacing: "1.5px",
-                                marginTop: "1rem"
-                            }}
-                            onClick={this.onLogoutClick}
-                            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                        >
-                            Logout
-            </button>
+                        <h4>Welcome, {user.username}!</h4>
+                        <button onClick={this.onLogoutClick}>Logout</button>
                     </div>
                 </div>
             </div>
