d70a7e7a9b25a5aa25b68900ec25180a18fe4d5e
cftgithub <chiafongt@gmail.com>
2020-08-07 13:36:03 -0600

    moved links to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,24 @@
 import React from "react";
+import References from "../References";
 import "./style.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
     return (
         <footer className="footer">
-            <div className="container">
-                <p className="text-center">Rockfish Conservation 2020</p>
-            </div>
+            <Container>
+
+            <Row>
+                <References />
+            </Row>
+            </Container>
+            <Container>
+                <Row>
+                    <Col>
+                        <p className="text-center">Rockfish Conservation 2020</p>
+                    </Col>
+                </Row>
+            </Container>
         </footer>
     );
 }
