dba1b2a40864bdf3118854abcf739a1685ad4599
cftgithub <chiafongt@gmail.com>
2020-08-06 21:18:56 -0600

    added path

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 // const db = require("./config/keys");
+const path = require("path");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
