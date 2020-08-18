0dd73bf5f78fc219669c891d356f4140be9e78c7
cftgithub <chiafongt@gmail.com>
2020-08-18 11:27:55 -0600

    fix routes

--- server.js
+++ server.js
@@ -13,19 +13,6 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
-
-if (process.env.NODE_ENV === "production") {
-  // Express will serve up production assets
-  app.use(express.static("client/build"));
-  // Express will serve up the front-end index.html file if it doesn't recognize the route
-  app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'client/build', 'index.html');
-    res.sendFile(index);
-  });
-}
-
-
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/creelDB";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, function (error) {
@@ -41,6 +28,19 @@ app.use(passport.initialize());
 require("./config/pass")(passport);
 
 app.use("/api/users", users);
+
+
+app.use(routes);
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  app.use(express.static("client/build"));
+  // Express will serve up the front-end index.html file if it doesn't recognize the route
+  app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'client/build', 'index.html');
+    res.sendFile(index);
+  });
+}
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
