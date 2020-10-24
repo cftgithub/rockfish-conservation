234b61164d227b7b06b22789cc5fcdc307d15cae
msyphus <mark.syphus@gmail.com>
2020-10-23 20:27:12 -0600

    commented out body-parser

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const routes = require("./routes");
@@ -8,8 +8,8 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
